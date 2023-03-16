f6cb7f6e53ba08a16153e39ca180644c0d1a4700
Thao Nguyen <thaonx.it@gmail.com>
2023-03-16 04:20:22 +0000

    generic sort

---
+++ xsort/sort_test.go
@@ -0,0 +1,44 @@
+package xsort
+
+import (
+	"fmt"
+	"testing"
+)
+
+type SortableObj struct {
+	Name string
+}
+
+func (c SortableObj) Less(other SortableObj) bool {
+	return c.Name < other.Name
+}
+
+func TestSort(t *testing.T) {
+	type args struct {
+		slices []SortableObj
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "test 1",
+			args: args{
+				slices: []SortableObj{
+					{
+						Name: "Name1",
+					},
+					{
+						Name: "Name2",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			SortableDesc(tt.args.slices)
+			fmt.Printf("%#v", tt.args.slices)
+		})
+	}
+}
